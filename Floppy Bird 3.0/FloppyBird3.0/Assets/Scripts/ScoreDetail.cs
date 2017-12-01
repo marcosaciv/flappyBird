a3a129060526e79704920fe5183759d054d5d658
cgfcarlos <carloscrg@hotmail.es>
2017-12-01 21:21:22 +0100

    Fixes

---
+++ Floppy Bird 3.0/FloppyBird3.0/Assets/Scripts/ScoreDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDetail : MonoBehaviour {
+
+    public Text rank;
+    public Text nombre;
+    public Text score;
+
+    public void setScore(int rank, string nombre, int score)
+    {
+        this.rank.text = rank.ToString();
+        this.nombre.text = nombre;
+        this.score.text = score.ToString();
+    }
+}
---
+++ Floppy Bird 3.0/FloppyBird3.0/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GameObject scoreDetail;
+    public RectTransform parent;
+
+    private bool touched = false;
+    private Transform buttonTouched;
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+        if (hit.collider != null && hit.transform.tag == "Back")
+        {
+            if (/*Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Stationary || */Input.GetMouseButtonDown(0))
+            {
+                hit.transform.position = new Vector2(hit.transform.position.x, hit.transform.position.y - 0.062f);
+                touched = true;
+                buttonTouched = hit.transform;
+            }
+
+            else if (/*Input.GetTouch(0).phase == TouchPhase.Ended ||*/ Input.GetMouseButtonUp(0))
+            {
+                SceneManager.LoadScene("Menu");
+            }
+
+        }
+        if (/*Input.GetTouch(0).phase == TouchPhase.Ended ||*/ Input.GetMouseButtonUp(0) && touched)
+        {
+            touched = false;
+            buttonTouched.position = new Vector2(buttonTouched.position.x, buttonTouched.position.y + 0.062f);
+            buttonTouched = null;
+
+        }
+    }
+
+    public class Jugador : IComparable<Jugador>
+    {
+        private string name;
+        private int puntuacion;
+
+        public Jugador(string name1, int puntuacion1)
+        {
+            this.name = name1;
+            this.puntuacion = puntuacion1;
+        }
+
+        public int getPuntuacion()
+        {
+            return this.puntuacion;
+        }
+
+
+        public int CompareTo(Jugador obj)
+        {
+            return this.getPuntuacion().CompareTo(obj.getPuntuacion());
+        }
+
+        public string ToString()
+        {
+            return name;
+        }
+    }
+
+    public string[] jugadores;
+    string crearJugadorURL = "http://www.floppy.site88.net/InsertarDatos.php";
+
+    IEnumerator Start()
+    {
+
+        WWW jugadoresData = new WWW("http://localhost/flappyBird/");
+        yield return jugadoresData;//para esperar a que se descarguen los datos de la pagina
+        string jugadoresString = jugadoresData.text;
+        // print(jugadoresString);
+        jugadores = jugadoresString.Split(';');
+
+        //ArrayList de jugadores
+        List<Jugador> jug = new List<Jugador>();
+        for (int i = 0; i < jugadores.Length - 1; i++)
+        {   
+            jug.Add(new Jugador(getDataValue(jugadores[i], "Nick:"), int.Parse(getDataValue(jugadores[i], "Puntuacion:"))));   
+        }
+
+        jug.Sort();
+        jug.Reverse();
+        for (int i = 0; i < jug.Count; i++)
+        {
+            GameObject score = Instantiate(scoreDetail);
+            score.transform.SetParent(parent);
+            score.GetComponent<ScoreDetail>().setScore(i+1, jug[i].ToString(),jug[i].getPuntuacion());
+
+            //GameObject b = Instantiate(bot);
+            //b.transform.SetParent(GameObject.Find("Rejilla").transform);
+            //b.GetComponentInChildren<Text>().text = jug[i].ToString();
+            //b.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        //PATRON -> Nick:x|Puntuacion:12|;Nick:y|Puntuacion:21
+
+        //comprobar puntuacion
+        //obtener puntuacion
+        //int puntuacion = PlayerPrefs.GetInt("Puntuacion");
+        //for (int i = 0; i < jugadores.Length - 1; i++)
+        //{
+        //    if (PlayerPrefs.GetString("nick") == getDataValue(jugadores[i], "Nick:"))
+        //    {
+        //        puntuacion = int.Parse(getDataValue(jugadores[i], "Puntuacion:"));
+        //    }
+        //}
+        //modificado
+
+        //PlayerPrefs.SetInt("Puntuacion", 0);
+
+
+
+    }
+
+    string getDataValue(string data, string index)//Nick o Puntuacion para index
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+        if (value.Contains("|"))
+        {
+            value = value.Remove(value.IndexOf("|"));
+        }
+        return value;
+    }
+
+    public void createJugador(string nick, int puntuacion)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("nickPost", nick);
+        form.AddField("puntuacionPost", puntuacion);
+
+        WWW www = new WWW(crearJugadorURL, form);
+    }
+
+}
